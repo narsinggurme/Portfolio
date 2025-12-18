1d426bc182f6f091dbf77b8041130f5e59ce04d2
narsinggurme <narsinggurme@gmail.com>
2025-12-18 21:47:15 +0530

    Deploy Angular portfolio on Render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyPortfolio';
+  isDark = true;
+  isMenuOpen = false;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.documentElement.classList.toggle('dark', this.isDark);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  ngOnInit() {
+    document.documentElement.classList.add('dark');
+  }
 }
